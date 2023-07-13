07b3a294237cd4c70410d9331afc324557ef7335
vergil-ong <865208597@qq.com>
2023-07-13 11:40:35 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello world 001"/>
+    <el-button type="success">Success</el-button>
   </div>
 </template>
 
